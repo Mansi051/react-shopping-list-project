ee47f09d9f74f8bddb40c6965576beb7f4b2c71e
Mansi051 <mansiarora23234@gmail.com>
2025-09-15 00:08:49 +0530

    commit

---
+++ shopping-list/src/App.js
@@ -0,0 +1,36 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import { useState } from "react";
+function App() {
+  const [items, setItems] = useState([]);
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+  const handleDeleteItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+  const handleDoneItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
+  return (
+    <div className="appWrapper">
+      <Header />
+      <Main
+        items={items}
+        handleAddItems={handleAddItems}
+        onDeleteItem={handleDeleteItem}
+        handleDoneItem={handleDoneItem}
+      />
+
+      <Footer items={items} />
+    </div>
+  );
+}
+
+export default App;
---
+++ shopping-list/src/components/Header.jsx
@@ -0,0 +1,24 @@
+//Create Component
+const Header = () => {
+  return (
+    <div className="headerWrapper">
+      <span>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+      </span>
+
+      <h1>Shopping List</h1>
+      <span>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-cart-shopping"></i>
+      </span>
+    </div>
+  );
+};
+export default Header;
